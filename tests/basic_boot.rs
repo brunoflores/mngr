fbf33ec7c51b6e106068ee6fef93f869489ad6bd
Bruno Flores <bruno@brunoflores.com.br>
2019-09-29 09:05:40 +1000

    Test println.

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,10 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(mngr::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use mngr::{println, serial_print, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -12,11 +13,14 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!()
-}
-
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}
+    mngr::test_panic_handler(info);
+}
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println...");
+    println!("test_println output");
+    serial_println!("[ok]");
 }
