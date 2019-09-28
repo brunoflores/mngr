19c9f9885a2b7bf9c4ebd2cbab25d7b8f600a3d7
Bruno Flores <bruno@brunoflores.com.br>
2019-09-28 21:04:34 +1000

    Implement println! macro.

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,6 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello!").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers {} {}", 42, 1.337)
-        .unwrap();
+    println!("Hello World{}", "!");
     loop {}
 }
