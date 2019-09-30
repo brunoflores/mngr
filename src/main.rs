b6b630b4c0491513f544e66d6e13b781848b1761
Bruno Flores <bruno@brunoflores.com.br>
2019-09-30 23:07:22 +1000

    Handle stack overflow exception.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,11 @@ pub extern "C" fn _start() -> ! {
 
     mngr::init();
 
+    fn stack_overflow() {
+        stack_overflow();
+    }
+    stack_overflow();
+
     #[cfg(test)]
     test_main();
 
