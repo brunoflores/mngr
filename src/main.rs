ed275055cb481b91f9e03aa8cbb48b01355b6651
Bruno Flores <bruno@brunoflores.com.br>
2019-09-30 20:50:40 +1000

    Handle breakpoint CPU exceptions.

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,14 @@ use mngr::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    mngr::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
+
+    println!("It did not crash!");
 
     loop {}
 }
