ff7d48198f253f73a8c249fbe758d56056ed6e7f
Bruno Flores <bruno@brunoflores.com.br>
2019-10-02 19:28:07 +1000

    Map from virt to phys.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,28 @@ use mngr::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    println!("Hello World{}", "!");
+    use x86_64::{structures::paging::MapperAllSizes, VirtAddr};
 
+    println!("Hello World{}", "!");
     mngr::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { mngr::memory::init(phys_mem_offset) };
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
