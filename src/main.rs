b55112fd14239f1d1269e8c4791f610864db3c71
Bruno Flores <bruno@brunoflores.com.br>
2019-10-02 20:49:49 +1000

    Map address 0 to the VGA buffer.

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,24 @@ use mngr::println;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::{structures::paging::MapperAllSizes, VirtAddr};
+    use mngr::memory;
+    use x86_64::{
+        structures::paging::{MapperAllSizes, Page},
+        VirtAddr,
+    };
 
     println!("Hello World{}", "!");
     mngr::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { mngr::memory::init(phys_mem_offset) };
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
+
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
